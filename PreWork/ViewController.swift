03eea85c77f55289946b4bf4e4bea89d3011a180
Claire Xia <minjie.xia@sjsu.edu>
2021-10-28 14:34:40 -0700

    test
    successfully created a tip calculator app

--- PreWork/ViewController.swift
+++ PreWork/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func calculateTip(_ sender: Any) {
+        // get bill amount from text field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        // get total tip by multiplying tip * tipPercentage
+        let tipPercentage = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // update tip amount label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        // update total amount
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 
 }
 
